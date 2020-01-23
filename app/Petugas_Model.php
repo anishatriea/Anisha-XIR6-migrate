5095bbb4c257e2a6bd896d54e966c2a4fa483326
Anisha <anisha_trie_27rpl@student.smktelkom-mlg.sch.id>
2020-01-23 10:19:19 +0700

    seeder

--- app/Petugas_Model.php
+++ app/Petugas_Model.php
@@ -2,11 +2,40 @@
 
 namespace App;
 
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class Petugas_Model extends Model
+class Petugas_Model extends Authenticatable implements JWTSubject
 {
-    protected $table="table_petugas";
-    protected $primaryKey="id";
-    public $timestamps= false;
+    use Notifiable;
+    protected $table = 'table_petugas';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'nama_petugas', 'alamat', 'telp', 'username', 'password', 'level',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,12 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('register', 'PetugasController@register');
+Route::post('login', 'PetugasController@login');
+
+Route::get('/', function(){
+    return Auth::user()->level;
+})->middleware('jwt.verify');
+
+Route::get('user', 'PetugasController@getAuthenticatedUser')->middleware('jwt.verify');
